82814f333e22aa0e23d16ca3389eefc8d26ccab6
RJLennon <robjohnlennon@gmail.com>
2024-02-20 23:17:28 -0500

    added code for viewing departments

--- index.js
+++ index.js
@@ -14,3 +14,49 @@ const db = mysql.createConnection(
   console.log(`Connected to the database.`)
 );
 
+//Create Menu Choices
+const viewDepartments = "View All Departments";
+const viewRoles = "View All Roles";
+const viewEmployees = "View All Employees";
+const addDepartment = "Add a Department";
+const addRole = "Add a Role";
+const addEmployee = "Add an Employee";
+const updateEmployee = "Update an Employee Role";
+
+function mainMenu() {
+  inquirer
+  .prompt([
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        viewDepartments,
+        viewRoles,
+        viewEmployees,
+        addDepartment,
+        addRole,
+        addEmployee,
+        updateEmployee,
+      ],
+      name: 'menu',
+    },
+  ])
+  .then((response) => {
+    switch(response.menu) {
+      case viewDepartments:
+        db.query('SELECT * FROM department', function (err, results) {
+          if (err) {
+            console.error('Error fetching departments', err);
+            return;
+          }
+          console.log("");
+          console.table(results);
+          console.log("");
+        });
+      break;
+      };
+  });
+  return;
+};
+
+mainMenu();
